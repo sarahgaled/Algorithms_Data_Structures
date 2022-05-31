319cd715c0550851538c7122218590e86b9d185d
sarahgaled <sarajessicagaled@email.com>
2022-05-30 20:11:26 -0400

    implementing classes Node and..

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,18 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null){
+        this.data = data
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    // constructor function is specifically made to intialize the linked list
+    constructor(){
+        this.head = null;
+    }
+}
 
 module.exports = { Node, LinkedList };
