4f7561b0ec9320582537c8ddc31d6686016c5fc3
sarahgaled <sarajessicagaled@email.com>
2022-05-31 18:45:05 -0400

    finding the midpoint with a linked list

--- midpoint/index.js
+++ midpoint/index.js
@@ -13,6 +13,21 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+    // define two variables, slow and fast that will look at the first element in our linkedlist
+    slow = list.getFirst()
+    fast = list.getFirst()
+    // write a while loop that says as long as the fast node has a node in front of it defined (next) and then a node if front of that defined, then
+    //  then we will continue to iterate by moving slow by one or to the next
+    while(fast.next && fast.next.next){
+        slow = slow.next
+        // and fast will jump forward by two
+        fast = fast.next.next
+    }
+    // if the fast variable doesnt have a node in front it, then we will exit the while loop
+    // so then we'll know that slow is point at the midpoint
+    // so we will then return slow after the while loop runs
+    return slow
+}
 
 module.exports = midpoint;
