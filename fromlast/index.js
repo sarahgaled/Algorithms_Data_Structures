013b27fb09caff5be84a966b2cee1a95e4d2ec0e
sarahgaled <sarajessicagaled@email.com>
2022-05-31 19:50:54 -0400

    from last linked list

--- fromlast/index.js
+++ fromlast/index.js
@@ -11,6 +11,27 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    // start by taking two pointers and set them both equal to the first node in the list
+    let slow = list.getFirst()
+    let fast = list.getFirst()
+    // then use a while loop to move fast through our linked list and move it forward n times
+    while(n > 0){
+        // advance fast through the linked list
+        fast = fast.next
+        // then subtract 1 from n
+        n--
+    }
+    // now advance both pointers forward by one at a time until fast hits the last node
+    // so while there is still some next node to visit
+    while(fast.next){
+        // advance slow forward by one as well as fast
+        slow = slow.next
+        fast = fast.next
+    }
+    // and the instance we hit the end of the chain, then slow must be n elements behind
+    //return slow
+    return slow
+}
 
 module.exports = fromLast;
